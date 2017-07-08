ab60fdb71f1f1127e46b69fadc02a3fe30f3272a
franco.sabino <franco.sabino@tecso.coop>
2017-07-08 11:41:23 -0300

    [DEV] - Generar la estructura principal del proyecto

---
+++ backend/devml-api/src/main/java/dev/fsabino/devml_api/App.java
@@ -0,0 +1,13 @@
+package dev.fsabino.devml_api;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}

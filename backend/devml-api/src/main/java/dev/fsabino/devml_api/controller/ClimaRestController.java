d601c4ffe1bfba09bf58ff79ff20976afc670d71
franco.sabino <franco.sabino@tecso.coop>
2017-07-09 21:26:04 -0300

    ClimaResController

--- backend/devml-api/src/main/java/dev/fsabino/devml_api/controller/ClimaRestController.java
+++ backend/devml-api/src/main/java/dev/fsabino/devml_api/controller/ClimaRestController.java
@@ -6,13 +6,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import dev.fsabino.devml_api.service.MeteorologiaService;
 
 @RestController
 @RequestMapping("/")
-public class ExerciseRestController {
+public class ClimaRestController {
 
 	@Autowired
 	@Qualifier("meteorologiaServiceImpl")
@@ -31,5 +32,13 @@ public class ExerciseRestController {
 			return new ResponseEntity<String>(HttpStatus.INTERNAL_SERVER_ERROR);
 		}
      }
-
+	
+	@GetMapping("/clima")
+    public ResponseEntity<String> getClimaDia(@RequestParam(required = true, value = "dia") int dia) {
+		try{
+			 return new ResponseEntity<String>("usted puso .."+ dia, HttpStatus.OK); 
+		}catch (Exception e) {
+			return new ResponseEntity<String>(HttpStatus.INTERNAL_SERVER_ERROR);
+		}
+    }
 }

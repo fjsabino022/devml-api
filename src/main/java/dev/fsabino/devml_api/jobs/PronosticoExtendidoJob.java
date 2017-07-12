46ca3278de13353b962f8279ce0db8f5f5ee8dde
franco.sabino <franco.sabino@tecso.coop>
2017-07-12 18:10:52 -0300

    Agregamos la clase PronosticoExtendidoJob

---
+++ src/main/java/dev/fsabino/devml_api/jobs/PronosticoExtendidoJob.java
@@ -0,0 +1,28 @@
+package dev.fsabino.devml_api.jobs;
+
+import java.util.Set;
+
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
+import org.springframework.stereotype.Component;
+
+import dev.fsabino.devml_api.repository.JedisRepository;
+
+@Component
+public class PronosticoExtendidoJob implements ApplicationListener<ApplicationReadyEvent>{
+	
+	  /**
+	   * This event is executed as late as conceivably possible to indicate that 
+	   * the application is ready to service requests.
+	   */
+	  @Override
+	  public void onApplicationEvent(final ApplicationReadyEvent event) {
+	 
+		  Set<String> keys = JedisRepository.getInstance().keys("*");
+		  for (String key : keys) {
+			  JedisRepository.getInstance().del(key);
+		  } 		  
+		  //JedisRepository.getInstance().set("agus", "agus");
+	    return;
+	  }
+}
